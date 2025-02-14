5d250a9789f051c477263255803c1bc46658ccd4
Jason Curl <jcurl@arcor.de>
2025-02-15 00:17:01 +0100

    QNX: Add ability for use messages
    All tools that are installed now have a usage message when compiled for QNX.
    
    Issue: OSDEV-82

--- cmake/modules/research/qnx.cmake
+++ cmake/modules/research/qnx.cmake
@@ -121,4 +121,90 @@ int main() { return 0; }
     endif()
 endfunction(_get_qnx_version_try_compile_c_cxx)
 
+function(target_use_msg TARGET USEFILE)
+    if(QNXNTO)
+        get_property(qnx_usemsg_found GLOBAL PROPERTY QNX_USEMSG_FOUND)
+        if(NOT DEFINED qnx_usemsg_found)
+            if(NOT QNX_USEMSG_EXECUTABLE)
+                set(QNX_USEMSG_EXECUTABLE usemsg)
+            endif()
+            if(NOT EXISTS ${QNX_USEMSG_EXECUTABLE})
+                find_program(_qnx_usemsg_executable ${QNX_USEMSG_EXECUTABLE})
+                if(_qnx_usemsg_executable)
+                    set(QNX_USEMSG_EXECUTABLE ${_qnx_usemsg_executable})
+                    unset(_qnx_usemsg_executable)
+                    set_property(GLOBAL PROPERTY QNX_USEMSG_FOUND TRUE)
+                    set_property(GLOBAL PROPERTY QNX_USEMSG_EXECUTABLE ${_qnx_usemsg_executable})
+                else()
+                    message(STATUS "usemsg: QNX ${QNX_USEMSG_EXECUTABLE} not found")
+                    set_property(GLOBAL PROPERTY QNX_USEMSG_FOUND FALSE)
+                endif()
+            endif()
+        else()
+            if(qnx_usemsg_found)
+                get_property(_qnx_usemsg_executable GLOBAL PROPERTY QNX_USEMSG_EXECUTABLE)
+                set(QNX_USEMSG_EXECUTABLE ${_qnx_usemsg_executable})
+            endif()
+        endif()
+
+        if(DEFINED QNX_USEMSG_EXECUTABLE)
+            set(_singleargs DESCRIPTION)
+            cmake_parse_arguments(PARSE_ARGV 2 arg "" "${_singleargs}" "")
+
+            site_name(HOSTNAME)
+            set(_USE_INFO_SOURCE "")
+            string(APPEND _USE_INFO_SOURCE "USER=$ENV{USER}\nHOST=${HOSTNAME}\nMYVERSION=x\n")
+            if(arg_DESCRIPTION)
+                string(APPEND _USE_INFO_SOURCE "DESCRIPTION=${arg_DESCRIPTION}")
+            endif()
+            file(WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${TARGET}_info.use" "${_USE_INFO_SOURCE}")
+            UNSET(_USE_INFO_SOURCE)
+
+            # This is very hacky.
+            #
+            # I need a way to run `usemsg` after building the target. But
+            # additionally to that, if the use message file is modified, I need
+            # a way to re-run the command.
+            #
+            # Because `add_custom_command(TARGET ... POST_BUILD)` ignores the
+            # DEPENDS command, and `add_executable(TARGET file.use)` is ignored
+            # because it's not a source file, modifying the `file.use` won't
+            # cause it to rebuild.
+            #
+            # So create an empty source file, add it as a dependency to the
+            # executable target. The empty source file is dependent on the
+            # `file.use` file. Thus when the use file is modified, the timestamp
+            # of the empty source file is updated. This causes the binary to be
+            # relinked (which unfortunately can add time to the build), but at
+            # least the POST_BUILD now is executed resulting in the use message
+            # being added.
+
+            # We could decide instead to copy the contents into a .c file and
+            # wrap it with a `#ifdef`, which QNX actually supports.
+            if(CMAKE_C_COMPILER_LOADED)
+                set(_qnx_usemsg_file "${USEFILE}.tmp.c")
+            elseif(CMAKE_CXX_COMPILER_LOADED)
+                set(_qnx_usemsg_file "${USEFILE}.tmp.cpp")
+            else()
+                message(FATAL_ERROR "TARGET_USE_MSG needs either C or CXX language enabled")
+            endif()
+            add_custom_command(
+                OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${_qnx_usemsg_file}"
+                DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${USEFILE}"
+                COMMAND ${CMAKE_COMMAND} -E touch "${CMAKE_CURRENT_BINARY_DIR}/${_qnx_usemsg_file}"
+                COMMENT ""
+            )
+            add_custom_command(
+                TARGET ${TARGET} POST_BUILD
+                COMMAND ${QNX_USEMSG_EXECUTABLE}
+                ARGS "$<TARGET_FILE:${TARGET}>" "${CMAKE_CURRENT_SOURCE_DIR}/${USEFILE}"
+                COMMAND ${QNX_USEMSG_EXECUTABLE}
+                ARGS -f "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${TARGET}_info.use" "$<TARGET_FILE:${TARGET}>"
+                VERBATIM
+            )
+            target_sources(${BINARY} PRIVATE ${_qnx_usemsg_file})
+        endif()
+    endif()
+endfunction()
+
 get_qnx_version()
