2a6aea9f4e2ad584512f65b2f6816dfee954b853
Jason Curl <jcurl@arcor.de>
2025-11-24 22:32:55 +0100

    libcpuid: class cpuid_dev
    Class to read from `/dev/cpu/N/cpuid` for querying the CPUID instruction.
    
    Issue: OSDEV-145

---
+++ qnx/tools/cpuid-x86/libcpuid/src/cpuid_dev.cpp
@@ -0,0 +1,26 @@
+#include "cpuid_dev.h"
+
+#include <unistd.h>
+
+#include <optional>
+
+namespace rjcp::cpuid {
+
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+auto cpuid_dev::cpuid(cpuidreg eax, cpuidreg ecx) -> std::optional<cpuid_res> {
+  if (!has_cpuid()) return std::nullopt;
+
+  struct cpuid_res res {};
+
+  off_t offset = eax | static_cast<off_t>(ecx) << 32;
+  auto result = pread(fd_, &res, 16, offset);
+  if (result <= 0) {
+    // We have an error, so we don't read any more values.
+    fd_ = -1;
+    return std::nullopt;
+  }
+
+  return res;
+}
+
+}  // namespace rjcp::cpuid
---
+++ qnx/tools/cpuid-x86/libcpuid/test/cpuid_dev_test.cpp
@@ -0,0 +1,117 @@
+#include "cpuid_dev.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <filesystem>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "ubench/file.h"
+
+auto get_fdpath(unsigned int cpunum) -> ubench::file::fdesc {
+  std::string path =
+      std::string{"/dev/cpu/"} + std::to_string(cpunum) + std::string{"/cpuid"};
+  std::filesystem::path dev{path};
+
+  if (!std::filesystem::exists(dev)) return ubench::file::fdesc{};
+
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
+  ubench::file::fdesc fd = open(dev.c_str(), O_RDONLY | O_CLOEXEC);
+  return fd;
+}
+
+TEST(cpuid_dev, has_cpuid_invalid) {
+  rjcp::cpuid::cpuid_dev cpu{-1};
+  ASSERT_FALSE(cpu.has_cpuid());
+}
+
+TEST(cpuid_dev, has_cpuid) {
+  auto fd = get_fdpath(0);
+  if (!fd) {
+    GTEST_SKIP() << "Couldn't open '/dev/cpu/0/cpuid'";
+  }
+
+  rjcp::cpuid::cpuid_dev cpu{fd};
+  ASSERT_TRUE(cpu.has_cpuid());
+  close(fd);
+}
+
+TEST(cpuid_dev, cpuid_zero) {
+  auto fd = get_fdpath(0);
+  if (!fd) {
+    GTEST_SKIP() << "Couldn't open '/dev/cpu/0/cpuid'";
+  }
+
+  rjcp::cpuid::cpuid_dev cpu{fd};
+  auto reg = cpu.cpuid(0, 0);
+
+  // Check the first register, whose result is expected to be not more than
+  // 0xFF leaves.
+  ASSERT_TRUE(reg.has_value());
+  if (reg) {
+    ASSERT_EQ(reg->eax & 0xFFFFFF00, 0);
+  }
+}
+
+TEST(cpuid_dev, cpuid_ext) {
+  auto fd = get_fdpath(0);
+  if (!fd) {
+    GTEST_SKIP() << "Couldn't open '/dev/cpu/0/cpuid'";
+  }
+
+  rjcp::cpuid::cpuid_dev cpu{fd};
+  auto reg = cpu.cpuid(0x80000000, 0);
+
+  // Check the extended register, whose result is expected to be not more than
+  // 0xFF leaves.
+  ASSERT_TRUE(reg.has_value());
+  if (reg) {
+    ASSERT_EQ(reg->eax & 0xFFFFFF00, 0x80000000);
+  }
+}
+
+TEST(cpuid_dev, move_ctor) {
+  auto fd = get_fdpath(0);
+  if (!fd) {
+    GTEST_SKIP() << "Couldn't open '/dev/cpu/0/cpuid'";
+  }
+
+  rjcp::cpuid::cpuid_dev cpu{fd};
+  ASSERT_TRUE(cpu.has_cpuid());
+
+  rjcp::cpuid::cpuid_dev cpu2{std::move(cpu)};
+
+  // We've moved, but after the move, we expect the object to at least indicate
+  // it is no longer valid. This is OK, because the classes are internal only to
+  // this implementation (and not public). It helps to show that when the
+  // objects that are moved out are destroyed, we have the correct behaviour.
+
+  // NOLINTNEXTLINE(bugprone-use-after-move,clang-analyzer-cplusplus.Move)
+  EXPECT_FALSE(cpu.has_cpuid());
+  EXPECT_TRUE(cpu2.has_cpuid());
+}
+
+TEST(cpuid_dev, move_assignment) {
+  auto fd = get_fdpath(0);
+  if (!fd) {
+    GTEST_SKIP() << "Couldn't open '/dev/cpu/0/cpuid'";
+  }
+
+  rjcp::cpuid::cpuid_dev cpu{fd};
+  ASSERT_TRUE(cpu.has_cpuid());
+
+  rjcp::cpuid::cpuid_dev cpu2;
+  cpu2 = std::move(cpu);
+
+  // We've moved, but after the move, we expect the object to at least indicate
+  // it is no longer valid. This is OK, because the classes are internal only to
+  // this implementation (and not public). It helps to show that when the
+  // objects that are moved out are destroyed, we have the correct behaviour.
+
+  // NOLINTNEXTLINE(bugprone-use-after-move,clang-analyzer-cplusplus.Move)
+  EXPECT_FALSE(cpu.has_cpuid());
+  EXPECT_TRUE(cpu2.has_cpuid());
+}
